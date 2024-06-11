92eaff58eea5062961bc4a7d3d533c461e16b531
afsanchez04 <afsanchez04@gmail.com>
2024-06-10 22:15:50 -0500

    feat: lightbox

---
+++ 08-lightbox/app.js
@@ -0,0 +1,26 @@
+//1.Variables
+const btnNumeros = document.querySelectorAll('.min');
+const flotante = document.querySelector('#flotante');
+const cuadroOpacidad = document.querySelector('#cuadro');
+const equis = document.querySelector('#equis');
+const btnSiguiente = document.querySelector('#sig');
+const btnAnterior = document.querySelector('#ant');
+const imgprincipal = document.querySelector('#imgprincipal');
+
+//2.Funciones
+function lightbox (e){
+    //console.log(e.target.src)
+    let currentImg = e.target.src;
+    flotante.style.top = '0vh';   
+    flotante.style.transitionDuration = '.5s';  
+    imgprincipal.setAttribute('src',currentImg)
+}
+function cerrar(){
+    flotante.style.top = '-100vh';   
+    flotante.style.transitionDuration = '.5s';   
+}
+//3.Eventos
+btnNumeros[0].addEventListener('click',lightbox)
+btnNumeros[1].addEventListener('click',lightbox)
+equis.addEventListener('click',cerrar);
+cuadroOpacidad.addEventListener('click',cerrar)
