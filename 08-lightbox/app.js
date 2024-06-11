dc9ace5f6564ab0c5b5186d8e4c249a0efd6f9a7
afsaanchez04 <afsanchez04@gmail.com>
2024-06-11 12:45:51 -0500

    feat: lightbox-finalizado

--- 08-lightbox/app.js
+++ 08-lightbox/app.js
@@ -19,8 +19,17 @@ function cerrar(){
     flotante.style.top = '-100vh';   
     flotante.style.transitionDuration = '.5s';   
 }
+function siguiente(){
+    imgprincipal.src="img2.jpg"
+}
+function anterior(){
+    imgprincipal.src="img1.jpg"
+}
+
 //3.Eventos
 btnNumeros[0].addEventListener('click',lightbox)
 btnNumeros[1].addEventListener('click',lightbox)
 equis.addEventListener('click',cerrar);
 cuadroOpacidad.addEventListener('click',cerrar)
+btnAnterior.addEventListener('click',anterior)
+btnSiguiente.addEventListener('click',siguiente)
